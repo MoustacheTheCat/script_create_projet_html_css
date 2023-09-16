1ebd04d8c8d28ce47db1ff6d3c1b523a490ad457
Moustache The Cat <joubert.mathieu753783@gmail.com>
2023-09-16 20:42:53 +0200

    version pour cette fois crée un projet en copiant une template crée au préalable

--- html_css/create_projet_html_css.sh
+++ html_css/create_projet_html_css.sh
@@ -1,7 +1,10 @@
-function create_projet_html() {
+#!/bin/bash
+
+function create_projet_html_css() {
     local name="$1"  
-    mkdir -p "$name"/{css,font,img/{logo,picture}} && touch "$name"/index.html >
+    cp -r -u /home/moustache/Documents/Donkey/Cours-théorique/cours-html-css/projet-html-css/template-html-css-responive "$name"
+    
 }
 
 # Utilisation de la fonction avec un nom de projet en argument
-create_projet_html "$1"
+create_projet_html_css "$1"
