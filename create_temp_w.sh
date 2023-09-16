2849baaadd8d767d536794eb9461ce974ba151b1
MoustacheTheCat <113124887+MoustacheTheCat@users.noreply.github.com>
2023-09-17 01:04:58 +0200

    Update create_temp_w.sh

--- create_temp_w.sh
+++ create_temp_w.sh
@@ -3,13 +3,13 @@ function create_temp_w() {
     local name="$2"
 
     if [ "$option" = "cnnb" ]; then
-        git clone https://github.com/MoustacheTheCat/template_html_css_responsive.git "$name"
+        git clone https://adresse_de_votre_repository.git "$name"
     elif [ "$option" = "cwnb" ]; then
-        git clone https://github.com/MoustacheTheCat/template_html_css_responsive_with_nav_bar.git "$name"
+        git clone https://adresse_de_votre_repository.git "$name"
     elif [ "$option" = "lnnb" ]; then
         mkdir -p "$name"/{css,font,img/{logo,picture}} && touch "$name"/index.html && touch "$name"/css/style.css
     elif [ "$option" = "lwnb" ]; then
-        cp -r -u /home/moustache/Documents/Donkey/Cours-théorique/cours-html-css/projet-html-css/template-html-css-responive "$name"
+        cp -r -u /emplacement_de_votre_template "$name"
     fi
 }
 
