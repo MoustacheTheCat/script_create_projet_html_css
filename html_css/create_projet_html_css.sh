ffa4e4e9c01503e8cacd12e7c4c9e7f92a9db13a
MoustacheTheCat <113124887+MoustacheTheCat@users.noreply.github.com>
2023-09-16 22:11:54 +0200

    Update create_projet_html_css.sh

--- html_css/create_projet_html_css.sh
+++ html_css/create_projet_html_css.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
-
-function create_projet_html_css() {
-    local name="$1"  
-    cp -r -u /home/moustache/Documents/Donkey/Cours-théorique/cours-html-css/projet-html-css/template-html-css-responive "$name"
-    
+function create_html_from_template() {
+        local name="$1"  
+                cp -r -u /home/moustache/Documents/Donkey/Cours-théorique/cours-html-css/projet-html-css/template-html-css-responive "$name"
 }
-
 # Utilisation de la fonction avec un nom de projet en argument
-create_projet_html_css "$1"
+create_html_from_template "$1"
+
