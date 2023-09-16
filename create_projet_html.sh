d010eba8e6ab1a1d6192fb4bbd43c7be529ac5f5
MoustacheTheCat <113124887+MoustacheTheCat@users.noreply.github.com>
2023-09-17 00:04:40 +0200

    Create create_projet_html.sh

---
+++ create_projet_html.sh
@@ -0,0 +1,7 @@
+function create_projet_html() {
+    local name="$1"  
+    mkdir -p "$name"/{css,font,img/{logo,picture}} && touch "$name"/index.html && touch "$name"/css/style.css
+}
+
+# Utilisation de la fonction avec un nom de projet en argument
+create_projet_html "$1"
