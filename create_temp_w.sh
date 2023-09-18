f6c9e7f9febf1d78ff4ffb48c414634333659bab
MoustacheTheCat <113124887+MoustacheTheCat@users.noreply.github.com>
2023-09-18 10:12:00 +0200

    Update create_temp_w.sh

--- create_temp_w.sh
+++ create_temp_w.sh
@@ -1,17 +1,41 @@
-function create_temp_w() {
+#!/bin/bash
+
+globct() {
     local option="$1"
     local name="$2"
+    local nb="$3"
+
+    if [ "$#" -ne 3 ]; then
+        echo "Usage: globct <option> <name> <nb>"
+        return 1
+    fi
 
     if [ "$option" = "cnnb" ]; then
-        git clone https://adresse_de_votre_repository.git "$name"
+        git clone https://votre_repository.git "$name"
     elif [ "$option" = "cwnb" ]; then
-        git clone https://adresse_de_votre_repository.git "$name"
+        git clone https://votre_repository.git "$name"
     elif [ "$option" = "lnnb" ]; then
-        mkdir -p "$name"/{css,font,img/{logo,picture}} && touch "$name"/index.html && touch "$name"/css/style.css
+        cp -r -u /localisation_de_votre_template/template-html-css-responive "$name"
     elif [ "$option" = "lwnb" ]; then
-        cp -r -u /emplacement_de_votre_template "$name"
+        cp -r -u /localisation_de_votre_template/template-html-css-responive-with-nav-bar "$name"
+    elif [ "$option" = "lnnbm" ]; then
+        cp -r -u /localisation_de_votre_template/template-html-css-responive-multi-page "$name"
+        for ((c=1; c<=nb; c++))
+        do  
+            cp -r -u /localisation_de_votre_template/template-html-css-responive-multi-page/html/page-1.html "$name"/html/page-"$c".html
+        done
+    elif [ "$option" = "lwnbm" ]; then
+        cp -r -u /localisation_de_votre_template/template-html-css-responive-with-nav-bar-multi-page "$name"
+        for ((c=1; c<=nb; c++))
+        do  
+            cp -r -u /localisation_de_votre_template/template-html-css-responive-with-nav-bar-multi-page/html/page-1.html "$name"/html/page-"$c".html
+        done
+    else
+        echo "Option invalide : $option"
+        return 1
     fi
 }
 
 # Utilisation de la fonction avec un nom de projet en argument
-create_temp_w "$1" "$2"
+globct "$1" "$2" "$3"
+
